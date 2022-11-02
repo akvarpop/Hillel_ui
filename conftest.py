547d7f125979af1c98ad3928bd255bec3de6739b
Popravka <104668775+akvarpop@users.noreply.github.com>
2022-11-02 14:34:53 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,28 +1,30 @@
 import pytest
+import os
 from selenium import webdriver
 from fake_useragent import UserAgent
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-# #useragent = UserAgent()
+'''options Chrome'''
 options = webdriver.ChromeOptions()
+options.add_argument('headless')
 options.add_argument('--ignore-ssl-errors=yes')
 options.add_argument('--ignore-certificate-errors')
-#options.add_argument(f"user-agent={useragent.cache}")
-#options.add_argument(f"user-agent={useragent.random}")
-#
-#
 
 
 @pytest.fixture(scope='session', autouse=True)
 def driver():
+    os.system("docker rm --force popravka_ua")
+    os.system(f"docker run -d --name popravka_ua -p 4444:4444 -p 5900:5900 seleniarm/standalone-chromium")
+    time.sleep(4)
     driver = webdriver.Remote(
-        command_executor='http://localhost:1414/wd/hub',
+        command_executor='http://localhost:4444/wd/hub',
         options=options
     )
     driver.maximize_window()
     yield driver
     driver.quit()
+    os.system("docker rm --force popravka_ua")
 
 
 """ Код для запуска с ПК  """
